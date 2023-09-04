90f2412072868da4587974b37d13a490f86ede40
jmtome <jtome@fi.uba.ar>
2023-09-04 16:48:22 -0300

    Added Gold Literal Color

--- MovieList/Extensions/UIColor+Extension.swift
+++ MovieList/Extensions/UIColor+Extension.swift
@@ -12,4 +12,5 @@ extension UIColor {
     static let softBlue = UIColor(red: 59/255, green: 67/255, blue: 103/255, alpha: 1)
     static let prussianBlue = UIColor(red: 29/255, green: 50/255, blue: 73/255, alpha: 1)
     static let softBlue2 = UIColor(red: 15/255, green: 22/255, blue: 29/255, alpha: 1)
+    static let gold = #colorLiteral(red: 0.9882352941, green: 0.7607843137, blue: 0, alpha: 1)
 }
