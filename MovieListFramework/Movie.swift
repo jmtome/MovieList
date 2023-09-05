e0da2c79b7a0e045a5e9edbc7d18e34ff3e06dd7
jmtome <jtome@fi.uba.ar>
2023-09-05 19:40:16 -0300

    Create the Movie model

---
+++ MovieListFramework/Movie.swift
@@ -0,0 +1,28 @@
+//
+//  Movie.swift
+//  MovieListFramework
+//
+//  Created by macbook on 05/09/2023.
+//
+
+import Foundation
+
+struct Movie {
+    let adult: Bool
+    let backdrop_path: String?
+    let genre_ids: [Int]
+    let id: UUID
+    let media_type: String?
+    let original_language: String
+    let original_title: String
+    let overview: String
+    let popularity: Double
+    let poster_path: String?
+    let release_date: String
+    let title: String
+    let video: Bool
+    let vote_average: Double
+    let vote_count: Int
+}
+
+
