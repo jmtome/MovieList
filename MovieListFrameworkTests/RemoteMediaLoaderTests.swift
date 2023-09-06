88e3b94dca6c5283341a70fb761117743af884ec
jmtome <jtome@fi.uba.ar>
2023-09-06 14:35:17 -0300

    Move test logic to a separate type 'HTTPClientSpy'

--- MovieListFrameworkTests/RemoteMediaLoaderTests.swift
+++ MovieListFrameworkTests/RemoteMediaLoaderTests.swift
@@ -9,32 +9,41 @@ import XCTest
 
 class RemoteMediaLoader {
     func load() {
-        HTTPClient.shared.requestedURL = URL(string: "https://a-url.com")
+        HTTPClient.shared.get(from: URL(string: "https://a-url.com")!)
     }
 }
 
 class HTTPClient {
-    static let shared = HTTPClient()
+    static var shared = HTTPClient()
+        
+    func get(from url: URL) {}
+}
+
+class HTTPClientSpy: HTTPClient {
+    override func get(from url: URL) {
+        requestedURL = url
+    }
     
-    private init() {}
     var requestedURL: URL?
 }
 
 final class RemoteMediaLoaderTests: XCTestCase {
 
     func test_init_doesNotRequestDataFromURL() {
-        let client = HTTPClient.shared
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
         let sut = RemoteMediaLoader()
     
         XCTAssertNil(client.requestedURL)
     }
     
     func test_load_requestDataFromURL() {
-        let client = HTTPClient.shared
+        let client = HTTPClientSpy()
+        HTTPClient.shared = client
         let sut = RemoteMediaLoader()
         
         sut.load()
-        
+         
         XCTAssertNotNil(client.requestedURL)
     }
 
