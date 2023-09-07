ac25620ca51cbb2649eabef80b7393a9f9a35e45
jmtome <jtome@fi.uba.ar>
2023-09-06 23:57:28 -0300

    Delivers connectivity error on client error

--- MovieListFrameworkTests/RemoteMediaLoaderTests.swift
+++ MovieListFrameworkTests/RemoteMediaLoaderTests.swift
@@ -35,6 +35,21 @@ final class RemoteMediaLoaderTests: XCTestCase {
         XCTAssertEqual(client.requestedURLs, [url, url])
     }
     
+    func test_load_deliversErrorOnClientError() {
+        // Given
+        let (sut, client) = makeSUT()
+        client.error = NSError(domain: "Test", code: 0)
+        var capturedError: RemoteMediaLoader.Error?
+        
+        // When
+        sut.load { error in
+            capturedError = error
+        }
+        
+        // Then
+        XCTAssertEqual(capturedError, .connectivity)
+    }
+    
     //MARK: - Helpers
     
     private func makeSUT(url: URL = URL(string: "https://a-url.com")!) -> (sut: RemoteMediaLoader, client: HTTPClientSpy) {
@@ -45,8 +60,12 @@ final class RemoteMediaLoaderTests: XCTestCase {
 
     private class HTTPClientSpy: HTTPClient {
         var requestedURLs = [URL]()
-
-        func get(from url: URL) {
+        var error: Error?
+        
+        func get(from url: URL, completion: @escaping (Error) -> Void) {
+            if let error = error {
+                completion(error)
+            }
             requestedURLs.append(url)
         }
         
