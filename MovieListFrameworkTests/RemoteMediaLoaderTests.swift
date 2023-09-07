271249a4c876a5c5422864f002d3592c32873d6e
jmtome <jtome@fi.uba.ar>
2023-09-07 19:31:55 -0300

    Delivers invalidData error on 200 HTTP response with invalid json

--- MovieListFrameworkTests/RemoteMediaLoaderTests.swift
+++ MovieListFrameworkTests/RemoteMediaLoaderTests.swift
@@ -62,7 +62,18 @@ final class RemoteMediaLoaderTests: XCTestCase {
             
             XCTAssertEqual(capturedErrors, [.invalidData])
         }
-       
+    }
+    
+    func test_load_delivers_ErrorOn200HTTPResponseWithInvalidJSON() {
+        let (sut, client) = makeSUT()
+        
+        var capturedErrors = [RemoteMediaLoader.Error]()
+        sut.load { capturedErrors.append($0) }
+        
+        let invalidJSON = Data("invalid JSON".utf8)
+        client.complete(withStatusCode: 200, data: invalidJSON)
+        
+        XCTAssertEqual(capturedErrors, [.invalidData])
     }
     
     //MARK: - Helpers
@@ -88,14 +99,14 @@ final class RemoteMediaLoaderTests: XCTestCase {
             messages[index].completion(.failure(error))
         }
         
-        func complete(withStatusCode code: Int, at index: Int = 0) {
+        func complete(withStatusCode code: Int, data: Data = Data(), at index: Int = 0) {
             let response = HTTPURLResponse(
                 url: requestedURLs[index],
                 statusCode: code,
                 httpVersion: nil,
                 headerFields: nil)!
             
-            messages[index].completion(.success(response))
+            messages[index].completion(.success(data, response))
         }
         
     }
