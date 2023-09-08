2c6ca95a7ccf70cbd056a43f0dd71f1b218ffcbe
jmtome <jtome@fi.uba.ar>
2023-09-08 20:10:17 -0300

    Guarantee we do not deliver a result (invoke the compeltion closure) after the RemoteMediaLoader instance has been deallocated

--- MovieListFrameworkTests/RemoteMediaLoaderTests.swift
+++ MovieListFrameworkTests/RemoteMediaLoaderTests.swift
@@ -119,6 +119,20 @@ final class RemoteMediaLoaderTests: XCTestCase {
         }
     }
     
+    func test_load_doesNotDeliverResultAfterSUTInstanceHasBeenDeallocated() {
+        let url = URL(string: "https://any-url.com")!
+        let client = HTTPClientSpy()
+        var sut: RemoteMediaLoader? = RemoteMediaLoader(url: url, client: client)
+        
+        var capturedResults = [RemoteMediaLoader.Result]()
+        sut?.load { capturedResults.append($0) }
+        
+        sut = nil
+        client.complete(withStatusCode: 200, data: makeItemsJSON([]))
+        
+        XCTAssertTrue(capturedResults.isEmpty)
+    }
+    
     //MARK: - Helpers
     
     private func makeSUT(url: URL = URL(string: "https://a-url.com")!,
