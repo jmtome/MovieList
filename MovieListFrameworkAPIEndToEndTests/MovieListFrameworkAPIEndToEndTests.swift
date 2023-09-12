efc6f6f495fe49223bf75a023dab6dab1ed6d1f4
jmtome <jtome@fi.uba.ar>
2023-09-12 17:30:00 -0300

    Use ephemeral URL session configuration to avoid sharing state across test executions (in-disk cache artifacts).

--- MovieListFrameworkAPIEndToEndTests/MovieListFrameworkAPIEndToEndTests.swift
+++ MovieListFrameworkAPIEndToEndTests/MovieListFrameworkAPIEndToEndTests.swift
@@ -30,7 +30,7 @@ final class MovieListFrameworkAPIEndToEndTests: XCTestCase {
     
     private func getFeedResult(file: StaticString = #file, line: UInt = #line) -> LoadMediaResult? {
         let testServerURL = URL(string: "https://gist.githubusercontent.com/jmtome/f09b007a6e46ae91148372a296e8c30d/raw/52267abea83aec0014db7388678e513695e8b6c2/E2ETestApiTMDB.json")!
-        let client = URLSessionHTTPClient()
+        let client = URLSessionHTTPClient(session: URLSession(configuration: .ephemeral))
         let loader = RemoteMediaLoader(url: testServerURL, client: client)
         trackForMemoryLeaks(client, file: file, line: line)
         trackForMemoryLeaks(loader, file: file, line: line)
