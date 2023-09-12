45d7b323e6985a13e115deaad456f996d2a2817c
jmtome <jtome@fi.uba.ar>
2023-09-12 20:03:30 -0300

    Save command requests cache deletion

--- MovieListFrameworkTests/Media Cache/CacheMediaUseCase.swift
+++ MovieListFrameworkTests/Media Cache/CacheMediaUseCase.swift
@@ -6,15 +6,26 @@
 //
 
 import XCTest
+import MovieListFramework
 
 class LocalMediaLoader {
+    private let store: MediaStore
+    
     init(store: MediaStore) {
-        
+        self.store = store
+    }
+    
+    func save(_ items: [MediaItem]) {
+        store.deleteCachedMedia()
     }
 }
 
 class MediaStore {
     var deleteCachedMediaCallCount = 0
+
+    func deleteCachedMedia() {
+        deleteCachedMediaCallCount += 1
+    }
 }
 
 class CacheMediaUseCase: XCTestCase {
@@ -24,5 +35,40 @@ class CacheMediaUseCase: XCTestCase {
         _ = LocalMediaLoader(store: store)
         
         XCTAssertEqual(store.deleteCachedMediaCallCount, 0)
+    }
+    
+    func test_save_requestsCacheDeletion() {
+        let store = MediaStore()
+        let sut = LocalMediaLoader(store: store)
+        let items = [uniqueItem(), uniqueItem()]
+        
+        sut.save(items)
+        
+        XCTAssertEqual(store.deleteCachedMediaCallCount, 1)
+    }
+    
+    
+    //MARK: - Helpers
+    
+    private func uniqueItem() -> MediaItem {
+        return MediaItem(adult: false,
+                         backdropPath: anyURL().absoluteString,
+                         genreIds: [],
+                         id: UUID().hashValue,
+                         mediaType: "any media",
+                         originalLanguage: "any language",
+                         originalTitle: "any og title",
+                         overview: "any overview",
+                         popularity: 0.0,
+                         posterPath: anyURL().absoluteString,
+                         releaseDate: "any date",
+                         title: "any title",
+                         video: false,
+                         voteAverage: 0.0,
+                         voteCount: 0)
+    }
+    
+    private func anyURL() -> URL {
+        return URL(string: "https://any-url.com")!
     }
 }
