5571442e49f0773fa6a538030539a94bd990e4f5
jmtome <jtome@fi.uba.ar>
2023-09-13 17:14:40 -0300

    Invert if logic to make code paths easier to follow

--- MovieListFrameworkTests/Media Cache/CacheMediaUseCase.swift
+++ MovieListFrameworkTests/Media Cache/CacheMediaUseCase.swift
@@ -21,13 +21,14 @@ class LocalMediaLoader {
     func save(_ items: [MediaItem], completion: @escaping (Error?) -> Void) {
         store.deleteCachedMedia { [weak self] error in
             guard let self = self else { return }
-            if error == nil {
+            
+            if let cacheDeletionError = error {
+                completion(cacheDeletionError)
+            } else {
                 self.store.insert(items, timestamp: self.currentDate()) { [weak self] error in
                     guard self != nil else { return }
                     completion(error)
                 }
-            } else {
-                completion(error)
             }
         }
     }
