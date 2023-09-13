80c31de3f7156a7e55d946eb98d70a50b76298de
jmtome <jtome@fi.uba.ar>
2023-09-13 20:49:45 -0300

    LocalMediaLoader does not message store upon creation (before loading the media from the cache store)

---
+++ MovieListFrameworkTests/Media Cache/LoadMediaFromCacheUseCaseTests.swift
@@ -0,0 +1,66 @@
+//
+//  LoadMediaFromCacheUseCaseTests.swift
+//  MovieListFrameworkTests
+//
+//  Created by macbook on 13/09/2023.
+//
+
+import XCTest
+import MovieListFramework
+
+final class LoadMediaFromCacheUseCaseTests: XCTestCase {
+    
+    func test_init_doesNotMessageStoreUponCreation() {
+        let (_, store) = makeSUT()
+        
+        XCTAssertEqual(store.receivedMessages, [])
+    }
+    
+    //MARK: - Helpers
+    
+    private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalMediaLoader, store: MediaStoreSpy) {
+        let store = MediaStoreSpy()
+        let sut = LocalMediaLoader(store: store, currentDate: currentDate)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return (sut, store)
+    }
+    
+    private class MediaStoreSpy: MediaStore {
+        enum ReceivedMessage: Equatable {
+            case deleteCachedFeed
+            case insert([LocalMediaItem], Date)
+        }
+        
+        private(set) var receivedMessages = [ReceivedMessage]()
+        
+        private var deletionCompletions = [DeletionCompletion]()
+        private var insertionCompletions = [InsertionCompletion]()
+        
+        func deleteCachedMedia(completion: @escaping DeletionCompletion) {
+            deletionCompletions.append(completion)
+            receivedMessages.append(.deleteCachedFeed)
+        }
+        
+        func completeDeletion(with error: Error, at index: Int = 0) {
+            deletionCompletions[index](error)
+        }
+        
+        func completeDeletionSuccessfully(at index: Int = 0) {
+            deletionCompletions[index](nil)
+        }
+        
+        func insert(_ items: [LocalMediaItem], timestamp: Date, completion: @escaping (InsertionCompletion)) {
+            insertionCompletions.append(completion)
+            receivedMessages.append(.insert(items, timestamp))
+        }
+        
+        func completeInsertion(with error: Error, at index: Int = 0) {
+            insertionCompletions[index](error)
+        }
+        
+        func completeInsertionSuccessfully(at index: Int = 0) {
+            insertionCompletions[index](nil)
+        }
+    }
+}
