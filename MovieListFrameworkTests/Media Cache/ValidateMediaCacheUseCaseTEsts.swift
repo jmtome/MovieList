ac99e192aa0ab29ad7e20b930238310a964df869
jmtome <jtome@fi.uba.ar>
2023-09-14 19:47:37 -0300

    Validate cache command, does not delete less Than Seven Days Old Cache

--- MovieListFrameworkTests/Media Cache/ValidateMediaCacheUseCaseTEsts.swift
+++ MovieListFrameworkTests/Media Cache/ValidateMediaCacheUseCaseTEsts.swift
@@ -34,6 +34,18 @@ final class ValidateMediaCacheUseCaseTEsts: XCTestCase {
         XCTAssertEqual(store.receivedMessages, [.retrieve])
     }
     
+    func test_validateCache_doesNotDeleteLessThanSevenDaysOldCache() {//
+        let items = uniqueItems()
+        let fixedCurrentDate = Date()
+        let lessThanSevenDaysOldTimestamp = fixedCurrentDate.adding(days: -7).adding(seconds: 1)
+        let (sut, store) = makeSUT(currentDate: { fixedCurrentDate })
+        
+        sut.validateCache()
+        store.completeRetrieval(with: items.local, timestamp: lessThanSevenDaysOldTimestamp)
+        
+        XCTAssertEqual(store.receivedMessages, [.retrieve])
+    }
+    
     //MARK: - Helperrs
     
     private func makeSUT(currentDate: @escaping () -> Date = Date.init, file: StaticString = #file, line: UInt = #line) -> (sut: LocalMediaLoader, store: MediaStoreSpy) {
@@ -47,5 +59,43 @@ final class ValidateMediaCacheUseCaseTEsts: XCTestCase {
     private func anyNSError() -> NSError{
         return NSError(domain: "any error", code: 0)
     }
+    
+    private func anyURL() -> URL {
+        return URL(string: "https://any-url.com")!
+    }
+    
+    private func uniqueItem() -> MediaItem {
+        return MediaItem(adult: false,
+                         backdropPath: anyURL().absoluteString,
+                         genreIds: [],
+                         id: UUID().hashValue,
+                         mediaType: "any media",
+                         originalLanguage: "any language",
+                         originalTitle: "any og title",
+                         overview: "any overview",
+                         popularity: 0.0,
+                         posterPath: anyURL().absoluteString,
+                         releaseDate: "any date",
+                         title: "any title",
+                         video: false,
+                         voteAverage: 0.0,
+                         voteCount: 0)
+    }
+    
+    private func uniqueItems() -> (models: [MediaItem], local: [LocalMediaItem]) {
+        let models = [uniqueItem(), uniqueItem()]
+        let local = models.map { LocalMediaItem(adult: $0.adult, backdropPath: $0.backdropPath, genreIds: $0.genreIds, id: $0.id, mediaType: $0.mediaType, originalLanguage: $0.originalLanguage, originalTitle: $0.originalTitle, overview: $0.overview, popularity: $0.popularity, posterPath: $0.posterPath, releaseDate: $0.releaseDate, title: $0.title, video: $0.video, voteAverage: $0.voteAverage, voteCount: $0.voteCount) }
+        return (models, local)
+    }
 
+}
+
+private extension Date {
+    func adding(days: Int) -> Date {
+        return Calendar(identifier: .gregorian).date(byAdding: .day, value: days, to: self)!
+    }
+    
+    func adding(seconds: TimeInterval) -> Date {
+        return self + seconds
+    }
 }
