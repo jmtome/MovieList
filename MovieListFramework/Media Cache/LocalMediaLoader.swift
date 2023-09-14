af45d8a96060fc5ba103d15ff10ddb21415338ae
jmtome <jtome@fi.uba.ar>
2023-09-14 20:51:37 -0300

    Make 'LocalMediaLoader' conform to 'MediaLoader'

--- MovieListFramework/Media Cache/LocalMediaLoader.swift
+++ MovieListFramework/Media Cache/LocalMediaLoader.swift
@@ -53,7 +53,7 @@ extension LocalMediaLoader {
     }
 }
 
-extension LocalMediaLoader {
+extension LocalMediaLoader: MediaLoader {
     public func load(completion: @escaping (LoadResult) -> Void) {
         store.retrieve { [weak self] result in
             guard let self = self else { return }
