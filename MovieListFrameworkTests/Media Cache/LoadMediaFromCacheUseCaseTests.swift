da488fbef7d1188a78823812961be1d8e86fa7e4
jmtome <jtome@fi.uba.ar>
2023-09-13 23:33:46 -0300

    Extract duplicate test code into a shared test helper method

--- MovieListFrameworkTests/Media Cache/LoadMediaFromCacheUseCaseTests.swift
+++ MovieListFrameworkTests/Media Cache/LoadMediaFromCacheUseCaseTests.swift
@@ -27,45 +27,18 @@ final class LoadMediaFromCacheUseCaseTests: XCTestCase {
     func test_load_failsOnRetrievalError() {
         let (sut, store) = makeSUT()
         let retrievalError = anyNSError()
-        let exp = expectation(description: "Wait for load completion")
-        
-        var receivedError: Error?
-        sut.load { result in
-            switch result {
-            case let .failure(error):
-                receivedError = error
-            default:
-                XCTFail("Expected failure, got \(result) instead")
-            }
-            
-            exp.fulfill()
+       
+        expect(sut, toCompleteWith: .failure(retrievalError)) {
+            store.completeRetrieval(with: retrievalError)
         }
-        
-        store.completeRetrieval(with: retrievalError)
-        wait(for: [exp], timeout: 1.0)
-        
-        XCTAssertEqual(receivedError as NSError?, retrievalError)
     }
     
     func test_load_deliversNoMediaItemsOnEmptyCache() {
         let (sut, store) = makeSUT()
-        let exp = expectation(description: "Wait for load completion")
-
-        var receivedMediaItems: [MediaItem]?
-        sut.load { result in
-            switch result {
-            case let .success(items):
-                receivedMediaItems = items
-            default:
-                XCTFail("Expected success, got \(result) instead")
-            }
-            exp.fulfill()
+       
+        expect(sut, toCompleteWith: .success([])) {
+            store.completeRetrievalWithEmptyCache()
         }
-
-        store.completeRetrievalWithEmptyCache()
-        wait(for: [exp], timeout: 1.0)
-
-        XCTAssertEqual(receivedMediaItems, [])
     }
     
     //MARK: - Helpers
@@ -76,6 +49,28 @@ final class LoadMediaFromCacheUseCaseTests: XCTestCase {
         trackForMemoryLeaks(store, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
         return (sut, store)
+    }
+    
+    private func expect(_ sut: LocalMediaLoader, toCompleteWith expectedResult: LocalMediaLoader.LoadResult, when action: () -> Void, file: StaticString = #file, line: UInt = #line) {
+        let exp = expectation(description: "Waiting for load completion")
+        
+        sut.load { receivedResult in
+            switch (receivedResult, expectedResult) {
+            case let (.success(receivedMediaItems), .success(expectedMediaItems)):
+                XCTAssertEqual(receivedMediaItems, expectedMediaItems, file: file, line: line)
+            
+            case let (.failure(receivedError as NSError), .failure(expectedError as NSError)):
+                XCTAssertEqual(receivedError, expectedError, file: file, line: line)
+                
+            default:
+                XCTFail("Expected result \(expectedResult), got \(receivedResult) instead", file: file, line: line)
+            }
+            
+            exp.fulfill()
+        }
+        
+        action()
+        wait(for: [exp], timeout: 1.0)
     }
     
     private func anyNSError() -> NSError{
