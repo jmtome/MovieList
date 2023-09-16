29b0f0e34d5f1b697639347bef5c4f9546e8db9b
jmtome <jtome@fi.uba.ar>
2023-09-16 17:53:35 -0300

    Extract duplicate delete test code into a reusable helper method

--- MovieListFrameworkTests/Media Cache/CodableMediaStoreTests.swift
+++ MovieListFrameworkTests/Media Cache/CodableMediaStoreTests.swift
@@ -203,14 +203,10 @@ final class CodableMediaStoreTests: XCTestCase {
     
     func test_delete_hasNoSideEffectsOnEmptyCache() {
         let sut = makeSUT()
-        let exp = expectation(description: "Wait for cache deletion")
         
-        sut.deleteCachedFeed { deletionError in
-            XCTAssertNil(deletionError, "Expected empty cache deletion to succeed")
-            exp.fulfill()
-        }
-        wait(for: [exp], timeout: 1.0)
+        let deletionError = deleteCache(from: sut)
         
+        XCTAssertNil(deletionError, "Expected empty cache deletion to succeed")
         expect(sut, toRetrieve: .empty)
     }
     
@@ -218,13 +214,9 @@ final class CodableMediaStoreTests: XCTestCase {
         let sut = makeSUT()
         insert((uniqueItems().local, Date()), to: sut)
         
-        let exp = expectation(description: "Wait for cache deletion")
-        sut.deleteCachedFeed { deletionError in
-            XCTAssertNil(deletionError, "Expected non-empty cache deletion to succeed")
-            exp.fulfill()
-        }
-        wait(for: [exp], timeout: 1.0)
+        let deletionError = deleteCache(from: sut)
         
+        XCTAssertNil(deletionError, "Expected existing cache to be deleted successfully")
         expect(sut, toRetrieve: .empty)
     }
     
@@ -239,6 +231,17 @@ final class CodableMediaStoreTests: XCTestCase {
     private func expect(_ sut: CodableMediaStore, toRetrieveTwice expectedResult: RetrieveCachedMediaItemsResult, file: StaticString = #file, line: UInt = #line) {
         expect(sut, toRetrieve: expectedResult, file: file, line: line)
         expect(sut, toRetrieve: expectedResult, file: file, line: line)
+    }
+    
+    private func deleteCache(from sut: CodableMediaStore) -> Error? {
+        let exp = expectation(description: "Wait for cache deletion")
+        var deletionError: Error?
+        sut.deleteCachedFeed { receivedDeletionError in
+            deletionError = receivedDeletionError
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1.0)
+        return deletionError
     }
     
     @discardableResult
