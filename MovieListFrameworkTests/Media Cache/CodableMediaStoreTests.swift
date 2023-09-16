c9809bfec6a02017a6e880843ef9c98ea298ed86
jmtome <jtome@fi.uba.ar>
2023-09-15 21:00:16 -0300

    Replace production store URL with a test-specific store url to avoid sharing state/ artifacts with other parts of the system (including other tests)

--- MovieListFrameworkTests/Media Cache/CodableMediaStoreTests.swift
+++ MovieListFrameworkTests/Media Cache/CodableMediaStoreTests.swift
@@ -89,13 +89,13 @@ final class CodableMediaStoreTests: XCTestCase {
     override func setUp() {
         super.setUp()
         
-        try? FileManager.default.removeItem(at: storeURL())
+        try? FileManager.default.removeItem(at: testSpecificStoreURL())
     }
     
     override func tearDown() {
         super.tearDown()
 
-        try? FileManager.default.removeItem(at: storeURL())
+        try? FileManager.default.removeItem(at: testSpecificStoreURL())
     }
     
     func test_retrieve_deliversEmptyOnEmptyCache() {
@@ -167,12 +167,12 @@ final class CodableMediaStoreTests: XCTestCase {
     //MARK: - Helpers
     
     private func makeSUT(file: StaticString = #file, line: UInt = #line) -> CodableMediaStore {
-        let sut = CodableMediaStore(storeURL: storeURL())
+        let sut = CodableMediaStore(storeURL: testSpecificStoreURL())
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
     }
     
-    private func storeURL() -> URL {
-        FileManager.default.urls(for: .documentDirectory, in: .userDomainMask).first!.appending(path: "media-items.store")
+    private func testSpecificStoreURL() -> URL {
+        FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!.appending(path: "\(type(of: self)).store")
     }
 }
