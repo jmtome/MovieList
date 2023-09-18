21e4f1ce987a99feab9067609470ad7b37af884a
jmtome <jtome@fi.uba.ar>
2023-09-18 19:39:00 -0300

    Extract FailableInsertMediaStoreSpecs checks into reusable assertions

---
+++ MovieListFrameworkTests/Media Cache/XCTestCase+FailableInsertMediaStoreSpecs.swift
@@ -0,0 +1,25 @@
+//
+//  XCTestCase+FailableInsertMediaStoreSpecs.swift
+//  MovieListFrameworkTests
+//
+//  Created by macbook on 18/09/2023.
+//
+
+import Foundation
+
+import XCTest
+import MovieListFramework
+
+extension FailableInsertMediaStoreSpecs where Self: XCTestCase {
+    func assertThatInsertDeliversErrorOnInsertionError(on sut: MediaStore, file: StaticString = #file, line: UInt = #line) {
+        let insertionError = insert((uniqueItems().local, Date()), to: sut)
+        
+        XCTAssertNotNil(insertionError, "Expected cache insertion to fail with an error", file: file, line: line)
+    }
+    
+    func assertThatInsertHasNoSideEffectsOnInsertionError(on sut: MediaStore, file: StaticString = #file, line: UInt = #line) {
+        insert((uniqueItems().local, Date()), to: sut)
+        
+        expect(sut, toRetrieve: .empty, file: file, line: line)
+    }
+}
