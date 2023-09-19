804117c229a7a8cc9cfec5814fff369055cf1b43
jmtome <jtome@fi.uba.ar>
2023-09-18 22:28:50 -0300

    Add empty CoreData feed store specs

---
+++ MovieListFrameworkTests/Media Cache/CoreDataMediaStoreTests.swift
@@ -0,0 +1,58 @@
+//
+//  CoreDataMediaStoreTests.swift
+//  MovieListFrameworkTests
+//
+//  Created by macbook on 18/09/2023.
+//
+
+import XCTest
+
+final class CoreDataMediaStoreTests: XCTestCase, MediaStoreSpecs {
+    func test_retrieve_deliversEmptyOnEmptyCache() {
+        
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnEmptyCache() {
+        
+    }
+    
+    func test_retrieve_deliversFoundValuesOnNonEmptyCache() {
+        
+    }
+    
+    func test_retrieve_hasNoSideEffectsOnNonEmptyCache() {
+        
+    }
+    
+    func test_insert_deliversNoErrorOnEmptyCache() {
+        
+    }
+    
+    func test_insert_deliversNoErrorOnNonEmptyCache() {
+        
+    }
+    
+    func test_insert_overridesPreviouslyInsertedCacheValues() {
+        
+    }
+    
+    func test_delete_deliversNoErrorOnEmptyCache() {
+        
+    }
+    
+    func test_delete_hasNoSideEffectsOnEmptyCache() {
+        
+    }
+    
+    func test_delete_deliversNoErrorOnNonEmptyCache() {
+        
+    }
+    
+    func test_delete_emptiesPreviouslyInsertedCache() {
+        
+    }
+    
+    func test_storeSideEffects_runSerially() {
+        
+    }
+}
