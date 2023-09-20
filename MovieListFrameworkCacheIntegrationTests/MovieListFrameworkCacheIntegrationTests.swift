cf6ab08f68a379d2b94bf5e63ed23fdc5ceba0b5
jmtome <jtome@fi.uba.ar>
2023-09-20 16:22:20 -0300

    'LocalMediaLoader' in integration with the 'CoreDataMediaStore' delivers no items on empty cache

--- MovieListFrameworkCacheIntegrationTests/MovieListFrameworkCacheIntegrationTests.swift
+++ MovieListFrameworkCacheIntegrationTests/MovieListFrameworkCacheIntegrationTests.swift
@@ -6,7 +6,46 @@
 //
 
 import XCTest
+import MovieListFramework
 
 final class MovieListFrameworkCacheIntegrationTests: XCTestCase {
     
+    func test_load_deliversNoItemsOnEmptyCache() {
+        let sut = makeSUT()
+        
+        let exp = expectation(description: "Wait for load completion")
+        sut.load { result in
+            switch result {
+            case let .success(mediaItems):
+                XCTAssertEqual(mediaItems, [], "Expected empty array of items")
+                
+            case let .failure(error):
+                XCTFail("Expected successfull mediaItems result, got \(error) instead")
+            }
+            
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1.0)
+    }
+    
+
+    //MARK: - Helpers
+    
+    private func makeSUT(file: StaticString = #file, line: UInt = #line) -> LocalMediaLoader {
+        let storeBundle = Bundle(for: CoreDataMediaStore.self)
+        let storeURL = testSpecificStoreURL()
+        let store = try! CoreDataMediaStore(storeURL: storeURL, bundle: storeBundle)
+        let sut = LocalMediaLoader(store: store, currentDate: Date.init)
+        trackForMemoryLeaks(store, file: file, line: line)
+        trackForMemoryLeaks(sut, file: file, line: line)
+        return sut
+    }
+    
+    private func testSpecificStoreURL() -> URL {
+        return cachesDirectory().appending(path: "\(type(of: self)).store")
+    }
+    
+    private func cachesDirectory() -> URL {
+        return FileManager.default.urls(for: .cachesDirectory, in: .userDomainMask).first!
+    }
 }
