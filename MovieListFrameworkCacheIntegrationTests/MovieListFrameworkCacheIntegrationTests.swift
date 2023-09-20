ddf92d0053759c507a3ab22ac7dd11ca78878b1a
jmtome <jtome@fi.uba.ar>
2023-09-20 17:51:18 -0300

    Extract duplicate cache load expectations into a shared helper method

--- MovieListFrameworkCacheIntegrationTests/MovieListFrameworkCacheIntegrationTests.swift
+++ MovieListFrameworkCacheIntegrationTests/MovieListFrameworkCacheIntegrationTests.swift
@@ -25,19 +25,7 @@ final class MovieListFrameworkCacheIntegrationTests: XCTestCase {
     func test_load_deliversNoItemsOnEmptyCache() {
         let sut = makeSUT()
         
-        let exp = expectation(description: "Wait for load completion")
-        sut.load { result in
-            switch result {
-            case let .success(mediaItems):
-                XCTAssertEqual(mediaItems, [], "Expected empty array of items")
-                
-            case let .failure(error):
-                XCTFail("Expected successfull mediaItems result, got \(error) instead")
-            }
-            
-            exp.fulfill()
-        }
-        wait(for: [exp], timeout: 1.0)
+        expect(sut, toLoad: [])
     }
     
     func test_load_deliversItemsSavedOnASeparateInstance() {
@@ -52,19 +40,7 @@ final class MovieListFrameworkCacheIntegrationTests: XCTestCase {
         }
         wait(for: [saveExp], timeout: 2.0)
         
-        let loadExp = expectation(description: "Wait for load completion")
-        sutToPerformLoad.load { loadResult in
-            switch loadResult {
-            case let .success(mediaItems):
-                XCTAssertEqual(mediaItems, items)
-                
-            case let .failure(error):
-                XCTFail("Expected successfull media items result, got \(error) instead")
-            }
-            
-            loadExp.fulfill()
-        }
-        wait(for: [loadExp], timeout: 1.0)
+        expect(sutToPerformLoad, toLoad: items)
     }
     
 
@@ -78,6 +54,22 @@ final class MovieListFrameworkCacheIntegrationTests: XCTestCase {
         trackForMemoryLeaks(store, file: file, line: line)
         trackForMemoryLeaks(sut, file: file, line: line)
         return sut
+    }
+    
+    private func expect(_ sut: LocalMediaLoader, toLoad expectedItems: [MediaItem], file: StaticString = #file, line: UInt = #line) {
+        let exp = expectation(description: "Wait for load completion")
+        sut.load { result in
+            switch result {
+            case let .success(mediaItems):
+                XCTAssertEqual(mediaItems, expectedItems, "Expected empty array of items", file: file, line: line)
+                
+            case let .failure(error):
+                XCTFail("Expected successfull mediaItems result, got \(error) instead", file: file, line: line)
+            }
+            
+            exp.fulfill()
+        }
+        wait(for: [exp], timeout: 1.0)
     }
     
     private func setupEmptyStoreState() {
