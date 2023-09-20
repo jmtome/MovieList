7aa6c9c5aa1d8ac66b1d9afb7ff349da0c844831
jmtome <jtome@fi.uba.ar>
2023-09-20 17:37:55 -0300

    'LocalMediaLoader' in integration with the 'CoreDateMediaStore' delivers items saved on separate instances, proving we correctly persist the data models to disk.

--- MovieListFrameworkCacheIntegrationTests/MovieListFrameworkCacheIntegrationTests.swift
+++ MovieListFrameworkCacheIntegrationTests/MovieListFrameworkCacheIntegrationTests.swift
@@ -40,6 +40,33 @@ final class MovieListFrameworkCacheIntegrationTests: XCTestCase {
         wait(for: [exp], timeout: 1.0)
     }
     
+    func test_load_deliversItemsSavedOnASeparateInstance() {
+        let sutToPerformSave = makeSUT()
+        let sutToPerformLoad = makeSUT()
+        let items = uniqueItems().models
+        
+        let saveExp = expectation(description: "Wait for save completion")
+        sutToPerformSave.save(items) { saveError in
+            XCTAssertNil(saveError, "Expected to save media items successfully")
+            saveExp.fulfill()
+        }
+        wait(for: [saveExp], timeout: 2.0)
+        
+        let loadExp = expectation(description: "Wait for load completion")
+        sutToPerformLoad.load { loadResult in
+            switch loadResult {
+            case let .success(mediaItems):
+                XCTAssertEqual(mediaItems, items)
+                
+            case let .failure(error):
+                XCTFail("Expected successfull media items result, got \(error) instead")
+            }
+            
+            loadExp.fulfill()
+        }
+        wait(for: [loadExp], timeout: 1.0)
+    }
+    
 
     //MARK: - Helpers
     
