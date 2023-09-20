effcff992a3b3c2ed7c81bc974ed8525d5b3cca2
jmtome <jtome@fi.uba.ar>
2023-09-20 16:07:04 -0300

    Add 'MovieListFrameworkCacheIntegrationTests' target to separate the potentially-slower cache integration tests from the fast unit/isolated tests

---
+++ MovieListFrameworkCacheIntegrationTests/MovieListFrameworkCacheIntegrationTests.swift
@@ -0,0 +1,12 @@
+//
+//  MovieListFrameworkCacheIntegrationTests.swift
+//  MovieListFrameworkCacheIntegrationTests
+//
+//  Created by macbook on 20/09/2023.
+//
+
+import XCTest
+
+final class MovieListFrameworkCacheIntegrationTests: XCTestCase {
+    
+}
