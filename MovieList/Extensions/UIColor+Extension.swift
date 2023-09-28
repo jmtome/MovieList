f52175d968a4dbeb78cf3d7b4f342ad50372ae29
jmtome <jtome@fi.uba.ar>
2023-09-28 15:45:41 -0300

    Further improvements to SwiftUI MovieList Cell

--- MovieList/Extensions/UIColor+Extension.swift
+++ MovieList/Extensions/UIColor+Extension.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-extension UIColor {
+public extension UIColor {
     static let softDark = UIColor(red: 11/255, green: 16/255, blue: 21/255, alpha: 1)
     static let softBlue = UIColor(red: 59/255, green: 67/255, blue: 103/255, alpha: 1)
     static let prussianBlue = UIColor(red: 29/255, green: 50/255, blue: 73/255, alpha: 1)
--- MovieList/Modules/MainModule/Views/SwiftUI/MediaCellView.swift
+++ MovieList/Modules/MainModule/Views/SwiftUI/MediaCellView.swift
@@ -9,26 +9,32 @@ import SwiftUI
 
 struct MediaCellView: View {
     
-    var title: String = "title"
+    var title: String = "Matrix Revolutions"
     var posterImage: Image = Image(systemName: "popcorn")
     var dateLabel: String = "2019-05-22"
     var description: String = "An exploratory dive into the deepest depths of the ocean of a daring research team spirals into chaos when a malevolent mining operation threatens their mission and forces them into a high-stakes battle for survival."
     var starRating: String = "★★★★☆"
     
     var body: some View {
-        HStack(alignment: .top) {
+        HStack(alignment: .center) {
             posterImage
                 .resizable()
                 .frame(height: 120)
-                .aspectRatio(0.125, contentMode: .fit)
+                .aspectRatio(0.12, contentMode: .fit)
                 .padding(.all, 5)
-                .background(.red)
+                .foregroundColor(Color(uiColor: UIColor.prussianBlue))
+                .background(.white)
                 .cornerRadius(20)
             VStack(alignment: .leading) {
                 Text(title)
-                    .font(.title)
+                    .font(.title3)
+                    .foregroundStyle(.gray)
+                    .padding(.top, 5)
+                    .padding(.bottom, 0)
                 Text(starRating)
                     .font(.subheadline)
+                    .foregroundStyle(.gray)
+                    .padding(.bottom, 2)
                 Text(dateLabel)
                     .font(.subheadline)
                 Text(description)
@@ -36,7 +42,9 @@ struct MediaCellView: View {
                     .font(.caption)
                     .lineLimit(3)
             }
+            .padding(.leading, 2)
             .padding(.bottom, 10)
+            .padding(.trailing, 10)
         }
         .background(Color(uiColor: .systemFill))
         .cornerRadius(10)
