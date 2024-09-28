16a13b9fce12e8f4a3301bd5d2730ac93b4865e4
jmtome <jtome@fi.uba.ar>
2024-09-28 17:20:15 -0300

    Added 3 new Views, SortMenuView, TrailingActionCircularImage, MainScreenView

---
+++ MovieList/Modules/MainScreenView.swift
@@ -0,0 +1,18 @@
+//
+//  MainScreenView.swift
+//  MovieList
+//
+//  Created by Juan Manuel Tome on 24/09/2024.
+//
+
+import SwiftUI
+
+struct MainScreenView: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    MainScreenView()
+}
---
+++ MovieList/Modules/TrailingActionCircularImage.swift
@@ -0,0 +1,18 @@
+//
+//  TrailingActionCircularImage.swift
+//  MovieList
+//
+//  Created by Juan Manuel Tome on 26/09/2024.
+//
+
+import SwiftUI
+
+struct TrailingActionCircularImage: View {
+    var body: some View {
+        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+    }
+}
+
+#Preview {
+    TrailingActionCircularImage()
+}
