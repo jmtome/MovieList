de443fed55396efdc19f8489e6975aecb7d08784
jmtome <jtome@fi.uba.ar>
2024-10-03 20:18:48 -0300

    Added new model for Person for crew members/actors

--- MovieList/Models/Model.swift
+++ MovieList/Models/Model.swift
@@ -265,26 +265,58 @@ protocol Media: Codable, Hashable {
     var mediaImages: [MediaImage]? { get set }
 }
 
+struct MovieCredits: Codable {
+    let cast: [Movie]
+}
 
-struct Person: Codable, Hashable {
+struct Person: Codable {
     let adult: Bool
+    let alsoKnownAs: [String]
+    let biography: String
+    let birthday: String
+    let deathday: String?
     let gender: Int
+    let homepage: String?
     let id: Int
+    let imdbID: String
     let knownForDepartment: String
     let name: String
-    let originalName: String
+    let placeOfBirth: String
     let popularity: Double
     let profilePath: String?
-    let knownFor: [Movie]
+    let movieCredits: MovieCredits
+    let images: ActorImages
+    
+    var profilePicturePath: String? {
+        guard let profilePath = profilePath else {
+            return nil
+        }
+        return "https://image.tmdb.org/t/p/w500/\(profilePath)"
+    }
     
     enum CodingKeys: String, CodingKey {
-        case adult, gender, id, name, popularity, knownFor
+        case adult
+        case alsoKnownAs = "also_known_as"
+        case biography
+        case birthday
+        case deathday
+        case gender
+        case homepage
+        case id
+        case imdbID = "imdb_id"
         case knownForDepartment = "known_for_department"
-        case originalName = "original_name"
+        case name
+        case placeOfBirth = "place_of_birth"
+        case popularity
         case profilePath = "profile_path"
+        case movieCredits = "movie_credits"
+        case images
     }
 }
 
+struct ActorImages: Codable {
+    let profiles: [MediaImage]
+}
 
 
 
