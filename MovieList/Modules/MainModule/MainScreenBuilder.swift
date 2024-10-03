0aa62c9e4b3f4d4e8126ec7195365690cc1a4f2c
jmtome <jtome@fi.uba.ar>
2024-10-03 20:19:50 -0300

    Added a HostingController to momentarily host the MainScreenView, and modified the builder

--- MovieList/Modules/MainModule/MainScreenBuilder.swift
+++ MovieList/Modules/MainModule/MainScreenBuilder.swift
@@ -6,22 +6,94 @@
 //
 
 import UIKit
+import MovieListFramework
 
 class MainScreenBuilder {
     static func build(favoritesRepository: FavoritesRepository, networkRepository: NetworkingService) -> UIViewController {
-        let viewController = MainScreenViewController()
+//        let viewController = MainScreenViewController()
+        let dummyData: [MediaViewModel] = {
+            var myData: [MediaViewModel] = []
+            for index in 0..<20 {
+                myData.append(MediaViewModel.viewModelFrom(mediaItem: MocchyItems.expectedItems(at: index)))
+            }
+            return myData
+        }()
+        let viewController = HostingController()
+        viewController.dataSource = dummyData
+        viewController.favoritesRepository = favoritesRepository
+        viewController.networkingService = networkRepository
         
-        let interactor = MainScreenInteractor(networkingService: networkRepository, favoritesRepository: favoritesRepository)
-        let router = MainScreenRouter(viewController)
         
+//        let interactor = MainScreenInteractor(networkingService: networkRepository, favoritesRepository: favoritesRepository)
+//        let router = MainScreenRouter(viewController)
+        
+//        let presenter = MainScreenPresenter(interactor: interactor, router: router)
+        
+//        interactor.output = presenter
+//        presenter.output = viewController
+//        
+//        viewController.presenter = presenter
+//        viewController.loadingPresenter = presenter
+        
+        return viewController
+    }
+}
+
+
+import UIKit
+import SwiftUI
+import MovieListFramework
+
+class HostingController: UIViewController {
+    var dataSource: [MediaViewModel] = []
+    var networkingService: NetworkingService!
+    var favoritesRepository: FavoritesRepository!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Sample data for testing
+        dataSource = createDummyData()
+        let router = MainScreenRouter(self)
+        
+        // Create the SwiftUI view
+        let interactor = MainScreenInteractor(networkingService: networkingService, favoritesRepository: favoritesRepository)
         let presenter = MainScreenPresenter(interactor: interactor, router: router)
         
         interactor.output = presenter
-        presenter.output = viewController
         
-        viewController.presenter = presenter
-        viewController.loadingPresenter = presenter
+        //Create favorites viper actors
+        let favoritesInteractor = FavoritesScreenInteractor(favoritesRepository: favoritesRepository)
+        let favoritesRouter = MainScreenRouter(self)
         
-        return viewController
+        let favoritesPresenter = FavoritesScreenPresenter(interactor: favoritesInteractor, router: favoritesRouter)
+        
+        favoritesInteractor.output = favoritesPresenter
+        
+        
+        let store = MediaViewStore(presenter: presenter, loadPresenter: presenter, favoritesPresenter: favoritesPresenter)
+        //        store.presenter = presenter
+        
+        presenter.output = store
+        favoritesPresenter.output = store
+
+        let mainScreenView = MainScreenView(dataSource: dataSource, isFavorited: MyTestPublisher(), newVM: store)
+        
+        // Create a UIHostingController with the SwiftUI view
+        let hostingController = UIHostingController(rootView: mainScreenView)
+        
+        // Add the hosting controller as a child
+        addChild(hostingController)
+        hostingController.view.frame = view.bounds // Match the frame
+        view.addSubview(hostingController.view)
+        hostingController.didMove(toParent: self)
+    }
+    
+    private func createDummyData() -> [MediaViewModel] {
+        var myData: [MediaViewModel] = []
+        for index in 0..<20 {
+            myData.append(MediaViewModel.viewModelFrom(mediaItem: MocchyItems.expectedItems(at: index)))
+        }
+        return myData
     }
 }
