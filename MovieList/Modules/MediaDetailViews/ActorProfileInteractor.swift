c7f9c123fcc3ff0487635ffe19bc01b6dce6ea93
jmtome <jtome@fi.uba.ar>
2024-10-03 20:08:01 -0300

    Added ActorProfileInteractor

--- MovieList/Modules/MediaDetailViews/ActorProfileInteractor.swift
+++ MovieList/Modules/MediaDetailViews/ActorProfileInteractor.swift
@@ -6,3 +6,39 @@
 //
 
 import Foundation
+
+protocol ActorProfileInteractorInputProtocol: AnyObject {
+    func fetchActorDetails(for actorId: Int)
+}
+protocol ActorProfileInteractorOutputProtocol: AnyObject {
+    func didReceiveActorDetails(_ actor: Person)
+    func didEncounterError(_ error: Error)
+}
+
+
+class ActorProfileInteractor {
+    weak var output: ActorProfileInteractorOutputProtocol?
+    
+    private let networkingService: NetworkingService
+    
+    init(networkingService: NetworkingService) {
+        self.networkingService = networkingService
+    }
+}
+
+extension ActorProfileInteractor: ActorProfileInteractorInputProtocol {
+    func fetchActorDetails(for actorId: Int) {
+        Task {
+            var actor: Person
+            do {
+                let actorData = try await networkingService.getActorDetails(for: actorId)
+                actor = try JSONDecoder().decode(Person.self, from: actorData)
+                output?.didReceiveActorDetails(actor)
+            } catch let error {
+                print("There was an error caught trying to fetch the Actor details for actorId: \(actorId)\n error: \(error)")
+                output?.didEncounterError(error)
+            }
+        }
+    }
+    
+}
