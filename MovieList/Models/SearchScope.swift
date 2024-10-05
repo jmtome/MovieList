8f4e62b096dee619946b698a51e1a4b6e9e26cb7
jmtome <jtome@fi.uba.ar>
2024-10-05 17:59:31 -0300

    Extracte SearchScope to a new file

--- MovieList/Models/SearchScope.swift
+++ MovieList/Models/SearchScope.swift
@@ -6,3 +6,31 @@
 //
 
 import Foundation
+
+enum SearchScope: Int {
+    case movies
+    case series
+    
+    var displayTitle: String {
+        switch self {
+        case .movies:
+            return "Movies"
+        case .series:
+            return "Series"
+        }
+    }
+    
+    var urlAppendix: String {
+        switch self {
+        case .movies: return "movies"
+        case .series: return "tv"
+        }
+    }
+    
+    var resultType: Codable.Type {
+        switch self {
+        case .movies: return Movie.self
+        case .series: return TVShow.self
+        }
+    }
+}
