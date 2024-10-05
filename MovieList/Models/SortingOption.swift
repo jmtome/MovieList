f9120137d0a6ed870015f9d201c03c292f6d81c9
jmtome <jtome@fi.uba.ar>
2024-10-05 17:59:19 -0300

    Extracted SortingOption to a new file

--- MovieList/Models/SortingOption.swift
+++ MovieList/Models/SortingOption.swift
@@ -6,3 +6,44 @@
 //
 
 import Foundation
+import UIKit
+
+enum SortingOption: SortableEnum {
+    var id: SortingOption { self }
+    
+    case relevance
+    case date
+    case rating
+    case title
+    
+    var title: String {
+        switch self {
+        case .relevance: return "Popularity"
+        case .date: return "Date"
+        case .rating: return "Rating"
+        case .title: return "Name"
+        }
+    }
+    
+    var identifier: String {
+        return "\(self)"
+    }
+
+    var image: UIImage {
+        switch self {
+        case .relevance: return UIImage(systemName: "chevron.up")!
+        case .date: return UIImage(systemName: "chevron.up")!
+        case .rating: return UIImage(systemName: "chevron.up")!
+        case .title: return UIImage(systemName: "chevron.up")!
+        }
+    }
+    
+    var swiftUIImageString: String {
+        switch self {
+        case .relevance: return "chevron.up"
+        case .date: return "chevron.up"
+        case .rating: return "chevron.up"
+        case .title: return "chevron.up"
+        }
+    }
+}
