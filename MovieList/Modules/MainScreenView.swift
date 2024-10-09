3a08be23d7318d0ad12787f34a1120960571e42a
jmtome <jtome@fi.uba.ar>
2024-10-09 19:21:58 -0300

    Extracted tabs into views and tab's views into views

--- MovieList/Modules/MainScreenView.swift
+++ MovieList/Modules/MainScreenView.swift
@@ -191,79 +191,16 @@ struct MainScreenView: View {
                 .pickerStyle(.segmented)
                 
                 TabView(selection: $tabSelection) {
-                    if homeMode == .list {
-                        MainScreenList(newVM: newVM, filteredData: filteredData)
-                            .transition(.slide) // Add a sliding transition
-                            .tabItem {
-                                Label("Main", systemImage: "film.fill")
-                            }
-                            .tag(0)
-                    } else {
-                        MainScreenGrid(newVM: newVM, filteredData: filteredData)
+                    MainTabView(newVM: newVM, filteredData: filteredData, homeMode: homeMode)
                         .tabItem {
-                            Label("Main", systemImage: "film.fill")
+                            Label("Movies", systemImage: "film")
                         }
                         .tag(0)
-                    }
-                    
-                    // Second tab
-                    if homeMode == .list {
-                        MainScreenFavoritesList(newVM: newVM, filteredFavoriteData: filteredFavoriteData)
-                            .tabItem {
-                                Label("Favorites", systemImage: "star.fill")
-                            }
-                            .tag(1)
-                    } else {
-                        let columns = [
-                            GridItem(.adaptive(minimum: 80))
-                        ]
-                        ScrollView(.vertical, showsIndicators: false) {
-                            LazyVGrid(columns: columns, alignment: .center, spacing: 20) {
-                                ForEach(filteredFavoriteData, id: \.id) { mediaItem in
-                                    NavigationLink(destination: MediaDetailView(store: newVM.buildStoreForDetails(with: mediaItem), media: mediaItem)) {
-                                        MediaCellGridView(media: mediaItem)
-                                            .onAppear {
-                                                impactFeedback.prepare()
-                                            }
-                                            .scaleEffect(isPressed ? 5 : 1.0) // Adjust the scale value for a more dramatic effect
-                                            .animation(.spring(response: 0.4, dampingFraction: 0.5), value: isPressed) // Control the speed and bounce of the animation
-                                            .gesture(
-                                                LongPressGesture(minimumDuration: 1.5) // Adjust the duration if needed
-                                                    .onChanged { _ in
-                                                        withAnimation {
-                                                            isPressed = true
-                                                        }
-                                                        impactFeedback.impactOccurred()
-                                                    }
-                                                    .onEnded { _ in
-                                                        withAnimation {
-                                                            isPressed = false
-                                                        }
-                                                    }
-                                            )
-                                            .contextMenu {
-                                                Button(action: {
-                                                    // Handle Favorite action
-                                                    newVM.handleFavorite(mediaItem)
-                                                }) {
-                                                    let isFavorite = newVM.isFavorite(mediaItem)
-                                                    Label(isFavorite ? "Unfavorite" : "Favorite", systemImage: isFavorite ? "star.fill" : "star")
-                                                }
-                                                Button(action: {
-                                                    // Handle Bookmark action
-                                                }) {
-                                                    Label("Bookmark", systemImage: "bookmark")
-                                                }
-                                            }
-                                    }
-                                }
-                            }
-                        }
+                    FavoritesTabView(newVM: newVM, filteredFavoriteData: filteredFavoriteData, homeMode: homeMode)
                         .tabItem {
                             Label("Favorites", systemImage: "star.fill")
                         }
                         .tag(1)
-                    }
                     Color.clear
                         .tabItem {
                             Label("Watch Later", systemImage: "bookmark.fill")
@@ -349,6 +286,33 @@ struct MainScreenView: View {
         .preferredColorScheme(.dark)
 }
 
+struct MainTabView: View {
+    @ObservedObject var newVM: MediaViewStore
+    let filteredData: [MediaViewModel]
+    let homeMode: HomeMode
+    
+    var body: some View {
+        if homeMode == .list {
+            MainScreenList(newVM: newVM, filteredData: filteredData)
+                .transition(.slide) // Add a sliding transition
+        } else {
+            MainScreenGrid(newVM: newVM, filteredData: filteredData)
+        }
+    }
+}
+struct FavoritesTabView: View {
+    @ObservedObject var newVM: MediaViewStore
+    let filteredFavoriteData: [MediaViewModel]
+    let homeMode: HomeMode
+    
+    var body: some View {
+        if homeMode == .list {
+            MainScreenFavoritesList(newVM: newVM, filteredFavoriteData: filteredFavoriteData)
+        } else {
+            MainScreenFavoritesGrid(newVM: newVM, filteredFavoriteData: filteredFavoriteData)
+        }
+    }
+}
 struct MainScreenGrid: View {
     @ObservedObject var newVM: MediaViewStore
     let filteredData: [MediaViewModel]
@@ -406,6 +370,63 @@ struct MainScreenGrid: View {
         
     }
 }
+
+struct MainScreenFavoritesGrid: View {
+    @ObservedObject var newVM: MediaViewStore
+    let filteredFavoriteData: [MediaViewModel]
+    @State var isPressed: Bool = false
+
+    private let impactFeedback = UIImpactFeedbackGenerator(style: .heavy)
+
+    var body: some View {
+        let columns = [
+            GridItem(.adaptive(minimum: 80))
+        ]
+        ScrollView(.vertical, showsIndicators: false) {
+            LazyVGrid(columns: columns, alignment: .center, spacing: 20) {
+                ForEach(filteredFavoriteData, id: \.id) { mediaItem in
+                    NavigationLink(destination: MediaDetailView(store: newVM.buildStoreForDetails(with: mediaItem), media: mediaItem)) {
+                        MediaCellGridView(media: mediaItem)
+                            .onAppear {
+                                impactFeedback.prepare()
+                            }
+                            .scaleEffect(isPressed ? 5 : 1.0) // Adjust the scale value for a more dramatic effect
+                            .animation(.spring(response: 0.4, dampingFraction: 0.5), value: isPressed) // Control the speed and bounce of the animation
+                            .gesture(
+                                LongPressGesture(minimumDuration: 1.5) // Adjust the duration if needed
+                                    .onChanged { _ in
+                                        withAnimation {
+                                            isPressed = true
+                                        }
+                                        impactFeedback.impactOccurred()
+                                    }
+                                    .onEnded { _ in
+                                        withAnimation {
+                                            isPressed = false
+                                        }
+                                    }
+                            )
+                            .contextMenu {
+                                Button(action: {
+                                    // Handle Favorite action
+                                    newVM.handleFavorite(mediaItem)
+                                }) {
+                                    let isFavorite = newVM.isFavorite(mediaItem)
+                                    Label(isFavorite ? "Unfavorite" : "Favorite", systemImage: isFavorite ? "star.fill" : "star")
+                                }
+                                Button(action: {
+                                    // Handle Bookmark action
+                                }) {
+                                    Label("Bookmark", systemImage: "bookmark")
+                                }
+                            }
+                    }
+                }
+            }
+        }
+    }
+}
+
 struct MainScreenList: View {
     @ObservedObject var newVM: MediaViewStore
     @State private var scrollViewPosition: Int? = 0
